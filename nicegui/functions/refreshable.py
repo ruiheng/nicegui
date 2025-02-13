9287ee253ab25ae7d5ab57d42b868928bb2953ba
Gilbert Fine <gilbert.fine@gmail.com>
2025-02-13 16:41:28 +0800

    fixup: the RefreshableContainer element may have been torn down, use 'is_deleted' to check instead

--- nicegui/functions/refreshable.py
+++ nicegui/functions/refreshable.py
@@ -124,7 +124,7 @@ class refreshable(Generic[_P, _T]):
         self.targets = [
             target
             for target in self.targets
-            if target.container.client.id in Client.instances and target.container.id in target.container.client.elements
+            if not target.container.is_deleted
         ]
 
 
